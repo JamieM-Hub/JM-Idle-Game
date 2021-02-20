a601f0ba5cf9fa54a8fdfceda1029c15fd252f1d
JamieH-Hub <jamiemckenzie7231@yahoo.co.uk>
2021-02-20 02:57:00 +0000

    refactor counter and data structure in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,53 @@
 $(document).ready(function () {
 
-    var counter = [],
-        upgradeUnlocked = [];
+    function Clicker(id, color, count, upgradeLevels) {
+        this.id = id;
+        this.color = color;
+        this.count = count;
+        this.upgradeLevels = upgradeLevels;
+    }
 
-initialise = (buttons, upgrades) => {
-    for (let i = 0; i < buttons; i++) {
-        counter[i] = 0;
-        console.log(counter[i]);
-        for (let j = 0; j < upgrades; j++) {
-            upgradeUnlocked[i, j] = false;
-            console.log(upgradeUnlocked[i, j]);
+    var clickerOne = new Clicker("One", "red", 0, [3, 6, 10]);
+    var clickerTwo = new Clicker("Two", "green", 0, [3, 6, 10]);
+    var clickerThree = new Clicker("Three", "green", 0, [3, 6, 10]);
+    var clickerFour = new Clicker("Four", "green", 0, [3, 6, 10]);
+    var clickerFive = new Clicker("Five", "green", 0, [3, 6, 10]);
+    var clickerSix = new Clicker("Six", "green", 0, [3, 6, 10]);
+    var clickerSeven = new Clicker("Seven", "green", 0, [3, 6, 10]);
+    var clickerEight = new Clicker("Eight", "green", 0, [3, 6, 10]);
+
+    let clickers = [clickerOne, clickerTwo, clickerThree, clickerFour, clickerFive, clickerSix, clickerSeven, clickerEight]
+    console.log(clickers);
+
+
+    // initialise = (buttons, upgrades) => {
+    //     for (let i = 0; i < buttons; i++) {
+    //         counter[i] = 0;
+    //         console.log(counter[i]);
+    //         for (let j = 0; j < upgrades; j++) {
+    //             upgradeUnlocked[i, j] = false;
+    //             console.log(upgradeUnlocked[i, j]);
+    //         }
+    //     }
+    // }
+    // initialise(8, 3);
+    // unlockUpgrade = (btn, unlock) => {
+    //     console.log("Upgrade " + unlock + " unlocked for Button " + btn + "!");
+    //     upgradeUnlocked[btn, unlock] = true;
+    // }
+
+    incrementCounter = (counter, i) => {
+        return counter + i;
+    }
+
+    $("button").click(function () {
+        var clickedButton = $(this).attr("id");
+        for (let i = 0; i < 8; i++) {
+            if (clickers[i].id === clickedButton) {
+                clickers[i].count = incrementCounter(clickers[i].count, 5);
+                $(this).text(clickers[i].count.toString())
+            }
         }
-    }
-}
-
-initialise(8, 3);
-
-unlockUpgrade = (btn, unlock) => {
-    console.log("Upgrade " + unlock + " unlocked for Button " + btn + "!");
-    upgradeUnlocked[btn, unlock] = true;
-}
-
-incrementCounter = (counter, i) => {
-    return counter + i;
-}
-
-$("button").click(function () {
-    var btnID = $(this).attr("id");
-    switch (btnID) {
-        case "b1":
-            counter[0] = incrementCounter(counter[0], 1);
-            $(this).text(counter[0].toString());
-    }
+    })
 
 });
