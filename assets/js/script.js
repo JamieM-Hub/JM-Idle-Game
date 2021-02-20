11151995d87f13dbc925f9109d39b1fa08724497
JamieH-Hub <jamiemckenzie7231@yahoo.co.uk>
2021-02-20 00:02:29 +0000

    add event listener to buttons

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var n1, n2, n3, n4, n5, n6, n7, n8;
+
+$(document).ready(function () {
+
+    $("button").click(function () {
+        var btn = $(this).attr("id");
+        console.log(btn, "click!");
+        
+    });
+
+});
